5382d36b69de507febdfe552749549ec1509052a
Elliot Jordan <ewjintl@icloud.com>
2020-01-24 15:47:00 -0800

    make me realize previous solution was limited to set of five, so abandoning that for new approach.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,33 @@
 // BACKEND LOGIC
+
 function range(num) {
+  var output = [];
   for (i = 0; i <= num; i++) {
-    // "contain" is the operative word here.
-    var display = "";
-    if ("1".includes(i) == true) {
-      display += "Beep!"; 
-      } else if ("2".includes(i) == true) {
-          display += "Boop!";
-        }
-    console.log( display || i);
+    output.push(i);    
   }
-  return 
+  return console.log(output);
 }
+
+
+
+
+
+
+// if ([i].includes(1)) {
+//   output += "Beep!"; 
+// //   // } else if ("2".includes(i) == true) {
+// //   //     display += "Boop!";
+//     }
+
+
+
 
 
 // FRONTEND LOGIC
 $(Document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
-    var endNumber = $("#userEndNumber").val();
+    var endNumber = parseInt($("#userEndNumber").val());
     console.log(endNumber);
 
     range(endNumber);
